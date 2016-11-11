1c40351506626a1a7e4a08f729b20392468e4e81
yesi <fab.grenouillet@gmail.com>
2016-11-11 21:36:37 +0400

    ios update

--- src/ios/CDVSumup.m
+++ src/ios/CDVSumup.m
@@ -12,72 +12,43 @@
 @implementation CDVSumup
 
 -(void) log:(CDVInvokedUrlCommand *)command {
-    [SumupSDK setupWithAPIKey:@"b3f3ae70-043d-4f23-821b-a5c9e2fefe76"];
-    
+    [SumupSDK setupWithAPIKey:@"YOUR_AFFILIATION_KEY"];
     [SumupSDK presentLoginFromViewController:self.viewController
-                                    animated:YES
-                             completionBlock:^(BOOL success, NSError *error) {
-                                 if (error) {
-                                     NSLog(@"%@", @"log error");
-                                     // errors are handled within the SDK, there should be no need
-                                     // for your app to display any error message
-                                 }
-                                 
-                                 
-                             }];
+        animated:YES
+        completionBlock:^(BOOL success, NSError *error) {
+        if (error) {
+            NSLog(@"%@", @"log error");
+        }
+    }];
 }
 
 -(void) pay:(CDVInvokedUrlCommand *)command {
-    
-    
-    
-                                 NSString *total = @"1.00";
-                                 
-                                 if ([total doubleValue] <= 0) {
-                                     return;
-                                 }
-                                 
-                                 SMPCheckoutRequest *request;
-                                 request = [SMPCheckoutRequest requestWithTotal:[NSDecimalNumber decimalNumberWithString:@"10.00"]
-                                                                          title:@"titre"
-                                                                   currencyCode:[[SumupSDK currentMerchant] currencyCode]
-                                                                 paymentOptions:SMPPaymentOptionAny];
-                                 
-                                 // the foreignTransactionID is an optional parameter and can be used
-                                 // to retrieve a transaction from SumUp's API. See -[SMPCheckoutRequest foreignTransactionID]
-                                 //[request setForeignTransactionID:[NSString stringWithFormat:@"your-unique-identifier-%@", [[NSProcessInfo processInfo] globallyUniqueString]]];
-                                 
-                                 [SumupSDK checkoutWithRequest:request fromViewController:self.viewController completion:^(SMPCheckoutResult *result, NSError *error) {
-                                     if ((error.code == SMPSumupSDKErrorAccountNotLoggedIn)) {
-                                         NSLog(@"%@", @"SMPSumupSDKErrorAccountNotLoggedIn");
-                                         return;
-                                     }
-                                     
-                                     if ([error.domain isEqualToString:SMPSumupSDKErrorDomain]) {
-                                         NSLog(@"%@", @"SMPSumupSDKErrorDomain");
-                                         return;
-                                     }
-                                     
-                                     
-                                     //NSLog([NSString stringWithFormat:@"%@ - %@", result.success ? @"Thank you" : @"No charge", result.transactionCode]);
-                                     //[self showResultsString:[NSString stringWithFormat:@"%@ - %@", result.success ? @"Thank you" : @"No charge", result.transactionCode]];
-                                     
-                                     if (result.success) {
-                                         NSLog(@"%@", @"ok");
-                                         
-                                     }
-                                     else { NSLog(@"%@", @"no success res"); }
-                                 }];
-                                 
-                                 // something went wrong checkout was not started
-                                 if (![SumupSDK checkoutInProgress]) {
-                                     NSLog(@"%@", @"failed to start");
-                                     //[self showResultsString:@"failed to start checkout"];
-                                 }
-                                 else { NSLog(@"%@", @"in progress"); }
-                                 
-    
-    
+    NSString *total = @"1.00";
+    if ([total doubleValue] <= 0) {
+        return;
+    }
+    SMPCheckoutRequest *request;
+    request = [SMPCheckoutRequest requestWithTotal:[NSDecimalNumber decimalNumberWithString:@"10.00"] title:@"titre"
+        currencyCode:[[SumupSDK currentMerchant] currencyCode]
+        paymentOptions:SMPPaymentOptionAny];
+        [SumupSDK checkoutWithRequest:request fromViewController:self.viewController completion:^(SMPCheckoutResult *result, NSError *error) {
+        if ((error.code == SMPSumupSDKErrorAccountNotLoggedIn)) {
+            NSLog(@"%@", @"SMPSumupSDKErrorAccountNotLoggedIn");
+            return;
+        }
+        if ([error.domain isEqualToString:SMPSumupSDKErrorDomain]) {
+            NSLog(@"%@", @"SMPSumupSDKErrorDomain");
+            return;
+        }
+        if (result.success) {
+            NSLog(@"%@", @"ok");
+        }
+        else { NSLog(@"%@", @"no success res"); }
+    }];
+    if (![SumupSDK checkoutInProgress]) {
+        NSLog(@"%@", @"failed to start");
+    }
+    else { NSLog(@"%@", @"in progress"); }
 }
 
 @end
