3ec939f116189607c39c0bd41801853d231694f3
yesi <fab.grenouillet@gmail.com>
2016-11-12 13:40:02 +0400

    ios update

---
+++ src/ios/SumupSDK.embeddedframework/SumupSDK.framework/Versions/A/Headers/SMPCheckoutResult.h
@@ -0,0 +1,31 @@
+//
+//  SMPCheckoutResult.h
+//  Cashier
+//
+//  Created by Lukas Mollidor on 10/06/15.
+//  Copyright (c) 2015 iosphere GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMPCheckoutResult : NSObject
+
+/**
+ *  A boolean indicating whether the checkout was sucessful.
+ */
+@property (readonly) BOOL success;
+
+/**
+ *  The transaction code to be used as a reference to this transaction.
+ *  Can be nil if the checkout failed in an early stage and did not reach SumUp's backend.
+ */
+@property (strong, readonly, nullable) NSString *transactionCode;
+
+/**
+ *  Additional information on the transaction like card information.
+ *  Can be nil if the checkout failed prior to payment type selection or card insertion.
+ *  Please see https://sumup.com/integration#TransactionHistoryModel for details.
+ */
+@property (strong, readonly, nullable) NSDictionary *additionalInfo;
+
+@end
